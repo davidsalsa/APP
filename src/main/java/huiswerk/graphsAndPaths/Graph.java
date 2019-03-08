e2b711d1d1186bc30623bf85e102e8785eece427
davidsalsa <davidsalsbach@hotmail.com>
2019-03-08 19:06:44 +0100

    dijkstra af

--- src/main/java/huiswerk/graphsAndPaths/Graph.java
+++ src/main/java/huiswerk/graphsAndPaths/Graph.java
@@ -4,36 +4,36 @@ import java.nio.file.Path;
 import java.util.*;
 
 public class Graph {
-    private HashMap<String, Vertex> vertexMap = new HashMap<>();
+    private HashMap<String, Node> vertexMap = new HashMap<>();
 
-    public void addVertex(String vertexName) {
-        if (vertexMap.get(vertexName) != null) {
-            throw new Error("There is already a vertex with that name!");
+    public void addNode(String nodeName) {
+        if (vertexMap.get(nodeName) != null) {
+            throw new Error("There is already a Node with that name!");
         }
 
-        vertexMap.put(vertexName, new Vertex(vertexName));
+        vertexMap.put(nodeName, new Node(nodeName));
     }
 
     public static final double INFINITY = Double.MAX_VALUE;
 
-    public void addEdgeBetweenVertex(String vertexName, String adjVertexName, double edgeCost) {
-        if (vertexMap.get(vertexName) == null || vertexMap.get(adjVertexName) == null) {
-            throw new Error("Cannot add an edge between the vertexes because one of the vertexes does not exist.");
+    public void addEdgeBetweenNodes(String nodeName, String adjNodeName, double edgeCost) {
+        if (vertexMap.get(nodeName) == null || vertexMap.get(adjNodeName) == null) {
+            throw new Error("Cannot add an edge between the nodes because one of the nodes does not exist.");
         }
 
-        Vertex vertexA = vertexMap.get(vertexName);
-        Vertex vertexB = vertexMap.get(adjVertexName);
-        Edge edge = new Edge(vertexB, edgeCost);
-        vertexA.addEdge(edge);
+        Node nodeA = vertexMap.get(nodeName);
+        Node nodeB = vertexMap.get(adjNodeName);
+        Edge edge = new Edge(nodeB, edgeCost);
+        nodeA.addEdge(edge);
     }
 
     public String toString() {
         StringBuilder arrayToString = new StringBuilder();
 
-        for (Map.Entry<String, Vertex> entry : vertexMap.entrySet()) {
-            Vertex vertex = entry.getValue();
-            for (Edge edge : vertex.getAdjEdges()) {
-                arrayToString.append(String.format("[%s] --{%s]--> [%s]", vertex.getName(),
+        for (Map.Entry<String, Node> entry : vertexMap.entrySet()) {
+            Node node = entry.getValue();
+            for (Edge edge : node.getAdjEdges()) {
+                arrayToString.append(String.format("[%s] --{%s]--> [%s]", node.getName(),
                         edge.getCost(), edge.getDest().getName())).append(System.getProperty("line.separator"));
             }
         }
@@ -42,6 +42,11 @@ public class Graph {
     }
 
     public void dijkstra(String startName){
+        PriorityQueue<Path> pq = new PriorityQueue<>();
+        Node start = vertexMap.get(startName);
+        if( start == null )
+            throw new NoSuchElementException( "Start vertex not found" );
+
 
     }
 
@@ -49,15 +54,23 @@ public class Graph {
         Graph graph = new Graph();
 
 
-        graph.addVertex("1");
-        graph.addVertex("2");
-        graph.addVertex("3");
-        graph.addVertex("4");
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
+        graph.addNode("D");
+        graph.addNode("E");
+        graph.addNode("F");
 
-        graph.addEdgeBetweenVertex("1", "2", 2);
-        graph.addEdgeBetweenVertex("1", "3", 3);
-        graph.addEdgeBetweenVertex("1", "4", 4);
+        graph.addEdgeBetweenNodes("A", "B", 2);
+        graph.addEdgeBetweenNodes("B", "F", 15);
+        graph.addEdgeBetweenNodes("B", "C", 4);
+        graph.addEdgeBetweenNodes("B", "D", 2);
+        graph.addEdgeBetweenNodes("D", "C", 3);
+        graph.addEdgeBetweenNodes("C", "E", 4);
+        graph.addEdgeBetweenNodes("E", "F", 3);
+        graph.addEdgeBetweenNodes("F", "D", 4);
         System.out.println(graph.toString());
+
     }
 }
 
@@ -71,10 +84,10 @@ class GraphException extends RuntimeException {
 
 
 class Edge {
-    public Vertex dest; // Second vertex in edge
+    public Node dest; // Second vertex in edge
     public double cost;
 
-    public Edge(Vertex dest, double cost) {
+    public Edge(Node dest, double cost) {
         this.dest = dest;
         this.cost = cost;
     }
@@ -83,16 +96,16 @@ class Edge {
         return cost;
     }
 
-    public Vertex getDest() {
+    public Node getDest() {
         return dest;
     }
 }
 
-class Vertex {
+class Node {
     private String name;
     private List<Edge> adjEdges; // Adjacent edges
 
-    public Vertex(String name) {
+    public Node(String name) {
         this.name = name;
         this.adjEdges = new LinkedList<>();
     }

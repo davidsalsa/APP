52a656e83db536863696c421f11a81167eba3a2d
David Salsbach <DB.Salsbach@student.han.nl>
2020-03-02 17:53:35 +0100

    implemented postorder, inorder and preorder, implemented remove function, implemented findmin and findmax

--- src/main/java/huiswerk2020/Trees/BinarySearchTree.java
+++ src/main/java/huiswerk2020/Trees/BinarySearchTree.java
@@ -9,8 +9,8 @@ public class BinarySearchTree<T extends Comparable<T>>  {
         root = null;
     }
 
-    public BinarySearchTree(Node<T> root){
-        this.root = root;
+    public BinarySearchTree(T rootValue){
+        this.root = new Node<>(rootValue);
     }
 
     private void insert(T key) {
@@ -36,6 +36,34 @@ public class BinarySearchTree<T extends Comparable<T>>  {
         return root;
     }
 
+    private void remove(T key)
+    {
+        root = removeRec(root, key);
+    }
+
+    private Node<T> removeRec(Node<T> root, T key){
+        if(root == null){
+            return null;
+        }
+        if(key.compareTo(root.key) < 0){
+            root.left = removeRec(root.left, key);
+        }else if(key.compareTo(root.key) > 0){
+            root.right = removeRec(root.right, key);
+        }else{
+            if(root.left == null && root.right == null){
+                root = null;
+            }else if(root.right == null){
+                root = root.left;
+            }else if(root.left == null){
+                root = root.right;
+            }else{
+                root.key = findMin(root.right).key;
+                root.right = removeRec(root.right, root.key);
+            }
+        }
+        return root;
+    }
+
 
     // only works with BST
     public Node findNode(T value){
@@ -43,6 +71,48 @@ public class BinarySearchTree<T extends Comparable<T>>  {
             return null;
         }
         return root.findNodeRecursively(value);
+    }
+
+    private void inOrder(Node root) {
+        if (root != null) {
+            inOrder(root.left);
+            System.out.println(root.key);
+            inOrder(root.right);
+        }
+    }
+
+    private void preOrder(Node node) {
+        if (node != null) {
+            System.out.println(" " + node.key);
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+    }
+
+    private void postOrder(Node node) {
+        if (node != null) {
+            postOrder(node.left);
+            postOrder(node.right);
+            System.out.println(" " + node.key);
+        }
+    }
+
+    Node<T> findMax(Node<T> root) {
+        if (root != null) {
+            if (root.right != null) {
+                root = findMax(root.right);
+            }
+        }
+        return root;
+    }
+
+    Node<T> findMin(Node<T> root) {
+        if (root != null) {
+            if (root.left != null) {
+                root = findMin(root.left);
+            }
+        }
+        return root;
     }
 
     public static void main(String[] args){
@@ -54,5 +124,16 @@ public class BinarySearchTree<T extends Comparable<T>>  {
         tree.insert(2);
         tree.insert(3);
 
+
+        tree.inOrder(tree.root);
+
+        System.out.println(" ");
+
+        tree.remove(4);
+        tree.remove(5);
+
+        tree.inOrder(tree.root);
+        System.out.println("minimum value: " + tree.findMin(tree.root).key);
+        System.out.println("maximum value: " + tree.findMax(tree.root).key);
     }
 }
